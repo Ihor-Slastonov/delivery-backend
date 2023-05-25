fcf947537319b578dff2de61ec86ca6efa75272e
Ihor-Slastonov <jammik000@gmail.com>
2023-05-25 14:53:54 +0300

    initial project

---
+++ server.js
@@ -0,0 +1,17 @@
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { DB_HOST, PORT } = process.env;
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    );
+    console.log('Database connection successful');
+  })
+  .catch();
