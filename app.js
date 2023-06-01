80e81da1961418ee768e2c9c62ac08588b38424d
Ihor-Slastonov <jammik000@gmail.com>
2023-06-01 19:11:35 +0300

    initial project

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 // Your routers
-// app.use('')
-// ////
+
+//-------------
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
